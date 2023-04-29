69e8625dc41b60461e140d85665a9af3b05d6fdc
Sviatoslav <105544922+forditt@users.noreply.github.com>
2023-04-30 01:31:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var cursor = true;
+    var speed = 600;  setInterval(() => {
+      if(cursor) {
+        document.getElementById('cursor').style.opacity = 0;
+        cursor = false;
+      }else {
+        document.getElementById('cursor').style.opacity = 1;
+        cursor = true;
+        }
+        }, speed);
